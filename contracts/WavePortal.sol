6735d5dc7f39d3725e5e2e88724a8bbecfd2036b
Vini Black <viniibl4ck@gmail.com>
2023-06-17 21:55:02 -0300

    fazendo a section 1 lesson 2

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Ueba, eu sou um contrato e eu sou inteligente");
+    }
+}
