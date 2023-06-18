03276e72eda8229a998698c81b440f969135e507
Vini Black <viniibl4ck@gmail.com>
2023-06-18 17:18:47 -0300

    fazendo a section 1 lesson 4

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Ueba, eu sou um contrato e eu sou inteligente");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s deu tchauzinho!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Temos um total de %d tchauzinhos!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,22 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContract = await hre.ethers.deployContract("WavePortal");
     await waveContract.waitForDeployment();
     console.log("Contract deployed to:", waveContract.target);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 main().catch((error) => {
