e77ea086caab147498071e1cbdc27c65cded637c
Vini Black <viniibl4ck@gmail.com>
2023-06-18 22:32:39 -0300

    fazendo a section 2 lesson 2

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "YOUR_ALCHEMY_API_URL",
+      accounts: ["YOUR_PRIVATE_SEPOLIA_ACCOUNT_KEY"],
+    },
+  },
 };
